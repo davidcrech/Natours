035b6de27afc854c0a816bd8342445e6ddfd4461
davidcrech <davidcrech220@gmail.com>
2025-05-16 17:33:12 -0300

    refactor: implement MVP logic on the project

--- server.js
+++ server.js
@@ -10,11 +10,13 @@ const DB = process.env.DATABASE.replace(
     process.env.DATABASE_PASSWORD
 );
 
-mongoose.connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-});
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log('db connection successful'))
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
