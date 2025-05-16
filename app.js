1af7f1b1384c777148b8807504ae3ccb334635ea
davidcrech <davidcrech220@gmail.com>
2025-05-16 16:12:25 -0300

    feat: connect mongodb

--- app.js
+++ app.js
@@ -2,15 +2,19 @@
 const morgan = require('morgan');
 const express = require('express');
 
-const tourRouter = require('./routes/tour');
-const userRouter = require('./routes/user');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // 1) MIDDLEWARES
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+};
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 // 4) ROUTES
 
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,26 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next) => {
+    if (+req.params.id > tours.length) {
+        return res.status(404).json({
+            status: "fail",
+            message: 'Invalid ID'
+        });
+    };
+    next();
+}
+
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).json({
+            status: "fail",
+            message: 'missing parameters'
+        });
+    };
+    next();
+};
+
 exports.getAllTours = (req, res) => {
     res.status(200).json({
         status: 'success',
@@ -17,13 +37,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
 
     const id = +req.params.id;
-
-        if (id > tours.length) {
-            return res.status(404).json({
-                status: "fail",
-                message: 'Invalid ID'
-            });
-        };
 
     const tour = tours.find(el => el.id === id);
     res.status(200).json({
@@ -52,12 +65,6 @@ exports.createTour = (req, res) => {
 }
 
 exports.updateTour = (req, res) => {
-    if (+req.params.id > tours.length) {
-        return res.status(404).json({
-            status: "fail",
-            message: 'Invalid ID'
-        });
-    };
 
     res.status(200).json({
         status: 'success',
@@ -68,12 +75,6 @@ exports.updateTour = (req, res) => {
 }
 
 exports.deleteTour = (req, res) => {
-    if (+req.params.id > tours.length) {
-        return res.status(404).json({
-            status: "fail",
-            message: 'Invalid ID'
-        });
-    };
 
     res.status(204).json({
         status: 'success',
