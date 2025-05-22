52aa0126ad03d3fef694f5898f54ee102afd9dd6
davidcrech <davidcrech220@gmail.com>
2025-05-22 14:30:58 -0300

    feat: add auth to user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,36 +1,44 @@
-const fs = require('fs');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+const User = require('./../models/userModel');
 
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined'
-  });
-};
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+    const users = await User.find();
+
+    // SEND RESPONSE
+    res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data: {
+            users
+        }
+    });
+});
 
 exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined'
-  });
+    res.status(500).json({
+        status: 'error',
+        message: 'this route is not yet defined'
+    });
 };
 
 exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined'
-  });
+    res.status(500).json({
+        status: 'error',
+        message: 'this route is not yet defined'
+    });
 };
 
 exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined'
-  });
+    res.status(500).json({
+        status: 'error',
+        message: 'this route is not yet defined'
+    });
 };
 
 exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined'
-  });
+    res.status(500).json({
+        status: 'error',
+        message: 'this route is not yet defined'
+    });
 };
