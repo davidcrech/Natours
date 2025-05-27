ef0d1b1d777ebf1bbeadf11f6e7f386342d723aa
davidcrech <davidcrech220@gmail.com>
2025-05-27 14:34:22 -0300

    feat: add RatingsQuantity & RatingsAverage

--- models/tourModel.js
+++ models/tourModel.js
@@ -40,7 +40,7 @@ const tourSchema = new mongoose.Schema(
             min: [1, 'Rating must be above 1.0'],
             max: [5, 'Rating must be below 5.0']
         },
-        ratingQuantity: {
+        ratingsQuantity: {
             type: Number,
             default: 0
         },
