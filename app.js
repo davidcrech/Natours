b3fc09e42503201a9d9e12437184907ce72eed75
davidcrech <davidcrech220@gmail.com>
2025-05-28 15:17:57 -0300

    feat: client side rendering with pug

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
@@ -11,10 +12,48 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+//alternative
+
+// Further HELMET configuration for Security Policy (CSP)
+const scriptSrcUrls = ['https://unpkg.com/', 'https://tile.openstreetmap.org'];
+const styleSrcUrls = [
+    'https://unpkg.com/',
+    'https://tile.openstreetmap.org',
+    'https://fonts.googleapis.com/'
+];
+const connectSrcUrls = ['https://unpkg.com', 'https://tile.openstreetmap.org'];
+const fontSrcUrls = ['fonts.googleapis.com', 'fonts.gstatic.com'];
+
+//set security http headers
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", 'blob:'],
+            objectSrc: [],
+            imgSrc: ["'self'", 'blob:', 'data:', 'https:'],
+            fontSrc: ["'self'", ...fontSrcUrls]
+        }
+    })
+);
+
+//alternative
+
 // 1) GLBOAL MIDDLEWARES
+
+// serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // security http headers
 app.use(helmet());
 
@@ -59,9 +98,6 @@ app.use(
     })
 );
 
-// serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // app.use((req, res, next) => {
 //     req.requestTime = new Date().toISOString();
 //     console.log(req.headers);
@@ -69,6 +105,8 @@ app.use(express.static(`${__dirname}/public`));
 // });
 
 // 4) ROUTES
+
+app.use('/', viewRouter);
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,7 @@ const createSendToken = (user, statusCode, res) => {
         httpOnly: true
     };
 
-    if (process.env.ENV_NODE === 'production') cookieOptions.secure = true;
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
     res.cookie('jwt', token, cookieOptions);
 
@@ -71,38 +71,86 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
-    // get the token, check if its exists
+    // 1) Getting token and check of it's there
     let token;
     if (
         req.headers.authorization &&
         req.headers.authorization.startsWith('Bearer')
     ) {
         token = req.headers.authorization.split(' ')[1];
-    }
-    if (!token) {
-        return next(new AppError('You are not logged in', 401));
-    }
-    // validate the token
-    const decoded = await promisify(jwt.verify)(
-        token,
-        process.env.JWT_SECRET,
-        () => {}
-    );
-    console.log(decoded);
-    // check if user still exists
-    const freshUser = await User.findById(decoded.id);
-    if (!freshUser) {
-        return next(new AppError('the user does no longer exists', 401));
-    }
-    // check if user changed password after jwt token was issued
-    if (freshUser.changedPasswordAfter(decoded.iat)) {
-        return next(new AppError('user recently changed password', 401));
+    } else if (req.cookies.jwt) {
+        token = req.cookies.jwt;
     }
 
-    // grant access to protected
-    req.user = freshUser;
+    if (!token) {
+        return next(
+            new AppError(
+                'You are not logged in! Please log in to get access.',
+                401
+            )
+        );
+    }
+
+    // 2) Verification token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    // 3) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+        return next(
+            new AppError(
+                'The user belonging to this token does no longer exist.',
+                401
+            )
+        );
+    }
+
+    // 4) Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next(
+            new AppError(
+                'User recently changed password! Please log in again.',
+                401
+            )
+        );
+    }
+
+    // GRANT ACCESS TO PROTECTED ROUTE
+    req.user = currentUser;
+    res.locals.user = currentUser;
     next();
 });
+
+// Only for rendered pages, no errors!
+exports.isLoggedIn = async (req, res, next) => {
+    if (req.cookies.jwt) {
+        try {
+            // 1) verify token
+            const decoded = await promisify(jwt.verify)(
+                req.cookies.jwt,
+                process.env.JWT_SECRET
+            );
+
+            // 2) Check if user still exists
+            const currentUser = await User.findById(decoded.id);
+            if (!currentUser) {
+                return next();
+            }
+
+            // 3) Check if user changed password after the token was issued
+            if (currentUser.changedPasswordAfter(decoded.iat)) {
+                return next();
+            }
+
+            // THERE IS A LOGGED IN USER
+            res.locals.user = currentUser;
+            return next();
+        } catch (err) {
+            return next();
+        }
+    }
+    next();
+};
 
 exports.restrictTo = (...roles) => {
     return (req, res, next) => {
@@ -208,3 +256,11 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
     // log user in, send JWT
     createSendToken(user, 200, res);
 });
+
+exports.logout = (req, res) => {
+    res.cookie('jwt', 'loggedout', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    });
+    res.status(200).json({ status: 'success' });
+};
