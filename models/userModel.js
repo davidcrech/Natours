e43b3f4ccbbe2969605bd8797ddded5e35f12a69
davidcrech <davidcrech220@gmail.com>
2025-05-29 17:07:17 -0300

    feat: add change photo feature

--- models/userModel.js
+++ models/userModel.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         validate: [validator.isEmail, 'Please, provide your email']
     },
-    photo: String,
+    photo: { type: String, default: 'default.jpg' },
     role: {
         type: String,
         enum: ['user', 'guide', 'lead-guide', 'admin'],
